f97c77e76fac9367d0d4c48f29222c6ff3a9a645
Fabian <fabian.illanes@gmail.com>
2017-12-24 13:24:59 -0600

    clean up lines

--- new.py
+++ new.py
@@ -5,40 +5,33 @@ def test(sequence):
 	print("length of sequence[0] =",len(sequence[0]))
 	print("element of sequence[0][1] =",sequence[0][1])
 
-
-s=[[1,2,3],[4,5,6]]
-
-array=[1]*4
-print("array1=",array)
-
-array2=[]
-print("array2=",array2)
-
-array3=[3,4,5]
-print("array3 =",array3)
-
-print("max element of array3 is",max(array3))
-
-
-print("Is 1 > 3",1>3)
-
-
-#f=open('/Users/fillanes/test.txt')
-f=open('test.txt')
-print(f.read())
-
-f.seek(0)
-print(f.readlines())
-
 def commonCharacterCount(s1, s2):
     com = [min(s1.count(i),s2.count(i)) for i in set(s1)]
     return sum(com)
 
-a1="abca"
-a2="xyzbac"
+#declarations
+s=[[1,2,3],[4,5,6]]
+array=[1]*4
+array2=[]
+array3=[3,4,5]
+a1="abcaa"
+a2="xyzbaca"
+
+#print statements
+print("array1=",array)
+print("array2=",array2)
+print("array3 =",array3)
+print("max element of array3 is",max(array3))
+print("Is 1 > 3",1>3)
+f=open('test.txt')
+print(f.read())
+print(f.read())
+#f.seek(0)
+print(f.readlines())
+
 print("a1=",a1)
 print("a2=",a2)
-print(a1.count(a2))
-print(a2.count(a1))
-print(set(a1))
-print(set(a2))
+print("a1.count(a2)=",a1.count(a2))
+print("a2.count(a1)=",a2.count(a1))
+print("set(a1)=",set(a1))
+print("set(a2)=",set(a2))
