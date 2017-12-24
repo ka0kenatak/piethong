4509fc9ed3332232d09ac6c1c528369ad093c1f7
Fabian <fabian.illanes@gmail.com>
2017-12-24 13:49:17 -0600

    next change

--- new.py
+++ new.py
@@ -10,6 +10,7 @@ def commonCharacterCount(s1, s2):
     return sum(com)
 
 #declarations
+f=open('test.txt')
 s=[[1,2,3],[4,5,6]]
 array=[1]*4
 array2=[]
@@ -18,20 +19,24 @@ a1="abcaa"
 a2="xyzbaca"
 
 #print statements
+print("f.read()=",f.read())
+#uncomment to re-read lines from text.txt
+#f.seek(0)
+print("f.read()=",f.read())
+print("f.readlines()=",f.readlines())
+print("s=",s)
 print("array1=",array)
 print("array2=",array2)
 print("array3 =",array3)
-print("max element of array3 is",max(array3))
 print("Is 1 > 3",1>3)
-f=open('test.txt')
-print(f.read())
-print(f.read())
-#f.seek(0)
-print(f.readlines())
-
+print("\n")
 print("a1=",a1)
 print("a2=",a2)
-print("a1.count(a2)=",a1.count(a2))
-print("a2.count(a1)=",a2.count(a1))
+print("a1.count('a')=",a1.count('a'))
+print("a2.count('x')=",a2.count('x'))
 print("set(a1)=",set(a1))
 print("set(a2)=",set(a2))
+print("min(array3)=",min(array3))
+print("min(s)=",min(s))
+print("max(array3)=",max(array3))
+print("max(s)=",max(s))
