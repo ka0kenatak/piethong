0bf53d5098c17adedfefceb1d88ef1aa8ecc09c6
Fabian Illanes <fillanes@rocketsoftware.com>
2017-12-25 23:23:43 -0600

    more lines

--- file.py
+++ file.py
@@ -1,21 +1,31 @@
 #functions definitions
 def reverseParentheses(s):
     a,b=s.index('('),s.index(')')
-    print(a,b)
-    newString=(s[a+1:b])
-    newList=[]
+    print("a and b =",a,b)
+
+    oldString=(s[a+1:b])
+    print("oldString =",oldString)
+
+    newString=oldString[::-1]
+    print("newString =",newString)
+
+    print((s.replace(oldString,newString)))
+    print("s =",s)
+
+    '''newList=[]
     for n in newString:
         newList.append(n)
-    newList.reverse()
-    print(newList)
-    return(newString)
+    newList.reverse()'''
+
+    #print("newList =",newList)
+    #return(s.replace(oldString,newString))
 
 
 #declarations
 test="a(bc)de"
 
 #funcation calls
-print(reverseParentheses(test))
+reverseParentheses(test)
 
 #print statements
 print("test string is",test)
