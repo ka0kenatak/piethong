cb3bd0952866488e35cb8bba512c6ec3d77462c3
Fabian Illanes <fillanes@rocketsoftware.com>
2018-01-06 14:57:48 -0600

    new lines

--- alternatingSums.py
+++ alternatingSums.py
@@ -1,2 +1,20 @@
 def alternatingSums(a):
-    
+    i=0
+    z=[0,0]
+    g1=[]
+    g2=[]
+    while(i <len(a)):
+
+
+
+
+
+
+
+#declaration
+test1=[50, 60, 60, 45, 70]
+test2=[100, 50]
+test3=[80]
+
+
+#function calls
