8e5c9f17ea1ba104eae34114d72cc6f093391f24
Fabian Illanes <fillanes@rocketsoftware.com>
2018-01-08 11:39:38 -0600

    new lines

--- addBorder.py
+++ addBorder.py
@@ -1,2 +1,15 @@
 def addBorder(picture):
-    
+    z=[""]*(len(picture[0])+2)
+    print(z)
+    #return(picture)
+
+
+
+#declarations
+test1=["abc","ded"]
+test2=["a"]
+
+
+#function calls
+addBorder(test1)
+addBorder(test2)
