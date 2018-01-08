90808d652e2adc4f8b6a966005c7d22df0c8c540
Fabian Illanes <fillanes@rocketsoftware.com>
2018-01-08 10:49:50 -0600

    done

--- alternatingSums.py
+++ alternatingSums.py
@@ -1,19 +1,21 @@
 def alternatingSums(a):
     z=[0,0]
-    if(len(z) == 1):
+    print("a =",a)
+
+    while(len(a)>0):
+        if(len(a) == 1):
         #return(z[0]=(z[0]+a[0]))
-        print(z[0]=(z[0]+a[0]))
-    else:
-        while(len(z)!=2):
+            z[0]=(z[0]+a[0])
+            a.pop(0)
+            print("z=",z)
+        else:
             z[0]=(z[0]+a[0])
             z[1]=z[1]+a[1]
             a.pop(0)
-            a.pop(1)
-        #return(z)
-        print(z)
-
-
-
+            a.pop(0)
+            print("a =",a)
+    #return(z)
+    print("z final=",z)
 
 
 
@@ -24,3 +26,6 @@ test3=[80]
 
 
 #function calls
+alternatingSums(test1)
+alternatingSums(test2)
+alternatingSums(test3)
