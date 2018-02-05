f90401a15853bdb741d6c423cdf0d2d0f3960409
Fabian <fabian.illanes@gmail.com>
2018-02-04 17:58:57 -0700

    solution

--- arrayChange.py
+++ arrayChange.py
@@ -1,2 +1,11 @@
-def arrayChange(inputArray):
-    
+def arrayChange(?):
+    p, s = a[?], 0
+
+    for c ?? a[1:]:
+        if ? <= p:
+            s += p - ? + 1
+            c = p + ?
+
+        p = c
+
+    return ?    
