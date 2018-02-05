d75bd55abed2561919b8277e15077b35abeeeb1a
Fabian <fabian.illanes@gmail.com>
2018-02-04 18:47:19 -0700

    new lines

--- arrayChange.py
+++ arrayChange.py
@@ -1,12 +1,12 @@
 def arrayChange(inputArray):
-    #??????? x[i] to x[?+1].
-    #If x[i] < ?[i+1], pass.
+    #compare x[i] to x[i+1].
+    #If x[i] < x[i+1], pass.
     #Else, ???? the difference x[i]+?-x[i+1].  Add ????
     #difference to total and ???? set x[i+1] ?? x[i]+1
-    x, array = inputArray, 0
+    x, var = inputArray, 0
     for i in range(len(x)-1):
         a,b = x[i], x[i+1]
         if a >= b:
-            array += a + 1 - b
+            var += a + 1 - b
             x[i+1] = a + 1
-    return array
+    return var
