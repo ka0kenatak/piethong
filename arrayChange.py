ff615ab76ae4dc33513b3f15ff3173f24472068e
Fabian Illanes <fillanes@rocketsoftware.com>
2018-03-01 11:18:32 -0600

    final changes

--- arrayChange.py
+++ arrayChange.py
@@ -1,2 +1,8 @@
 def arrayChange(inputArray):
-    
+    x, array = inputArray, 0
+    for i in range(len(x)-1):
+        a,b = x[i], x[i+1]
+        if a >= b:
+            array += a + 1 - b
+            x[i+1] = a + 1
+    return array
