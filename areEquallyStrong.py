18d26aa326166035f0a540ea77039210109ce2a2
Fabian <fabian.illanes@gmail.com>
2018-05-06 21:55:30 -0500

    nailed it

--- areEquallyStrong.py
+++ areEquallyStrong.py
@@ -1,2 +1,17 @@
 def areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight):
-    
+    if(max(yourLeft,yourRight)==max(friendsLeft,friendsRight) and min(yourLeft,yourRight)==min(friendsLeft,friendsRight)):
+        return(True)
+    else:
+        return(False)
+
+
+#declarations
+test1a=10
+test1b=15
+test1c=10
+test1d=15
+
+
+
+#function calls
+areEquallyStrong(test1a,test1b,test1c,test1d)
