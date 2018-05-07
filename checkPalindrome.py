83dbeb2c1aedc6e0c9add66c102b80460f5887c1
Fabian <fabian.illanes@gmail.com>
2018-05-06 20:43:16 -0500

    new lines

--- checkPalindrome.py
+++ checkPalindrome.py
@@ -3,14 +3,14 @@ def checkPalindrome(inputString):
     x = int(length/2)
     if length/2 == int(length/2):
         #if inputString[:x] == inputString[x:length]:
-        #    return (True)         
+        #    return (True)
         if inputString[:x] == inputString[:x-1:-1]:
             return (True)
         else:
             return(False)
     else:
         #if inputString[:x] == inputString[x+1:length]:
-        #    return (True)         
+        #    return (True)
         if inputString[:x] == inputString[:x:-1]:
             return (True)
         else:
