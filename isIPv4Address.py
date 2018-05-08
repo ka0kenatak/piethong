da3ca2388b671289cd6567a6e513621af4041cd4
Fabian Illanes <fillanes@rocketsoftware.com>
2018-05-08 10:35:28 -0400

    new output lines

--- isIPv4Address.py
+++ isIPv4Address.py
@@ -1,2 +1,12 @@
 def isIPv4Address(inputString):
-    
+    array=[inputString]
+    print(inputString[0])
+    print(array[0])
+
+
+#declarations
+test="172.16.254.1"
+
+
+#function call
+isIPv4Address(test)
