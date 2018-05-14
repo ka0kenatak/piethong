7c08f5f03a96e18a1b254b2f54ab2aff6ac24313
Fabian Illanes <fillanes@rocketsoftware.com>
2018-05-14 13:23:59 -0400

    new lines

--- isIPv4Address.py
+++ isIPv4Address.py
@@ -1,24 +1,27 @@
 def isIPv4Address(inputString):
 
+    #Unit Test - verify if input is blank
+    def verifyBlank(input):
+        if not input:
+            return False
+        else:
+            return True
+    #Unit Test - verify if input is only numerical
+    def verifyAlpha(input):
+        return(input.isdigit())
+
     def convertArray(input):
         list=[]
         for i in input:
             list.append(i)
         return(list)
 
-    #unit tests
-    def verifyBlank(input):
-        if not input:
-            return False
-        else:
-            return True
-
-
-    a=convertArray(inputString)
+    '''a=convertArray(inputString)
     print(a)
     indices = [i for i, x in enumerate(inputString) if x == "."]
-    print(indices)
+    print(indices)'''
 
+    print(verifyAlpha(inputString))
 
 
 
@@ -27,7 +30,8 @@ def isIPv4Address(inputString):
 test1="172.16.254.1"
 test2="172162541"
 test3=""
+test4="172.a.13.34"
 
 
 #function call
-isIPv4Address(test1)
+isIPv4Address(test4)
