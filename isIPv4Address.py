d068b055327fc20a8d649187306a866fd9df6315
Fabian <fabian.illanes@gmail.com>
2018-05-27 02:54:25 -0500

    new lines

--- isIPv4Address.py
+++ isIPv4Address.py
@@ -10,10 +10,6 @@ def isIPv4Address(inputString):
     def verifyAlpha(input):
         return(input.isdigit())
 
-    #split up the octets
-    def separateOctets(input):
-        return()
-        
     #Unit Test - verify input is valid octets
     def verifyLimit(input):
         x=int(input)
@@ -22,31 +18,42 @@ def isIPv4Address(inputString):
         else:
             return(False)
 
+    #Convert input string into an array
     def convertArray(input):
         list=[]
         for i in input:
             list.append(i)
         return(list)
 
+    #Split up the octets
+    def separateOctets(input):
+        a=convertArray(inputString)
+        print(a)
+        indices = [i for i, x in enumerate(inputString) if x == "."]
+        indices.insert(0,0)
+        indices.append(len(a)-1)
+        print(indices)
 
-    a=convertArray(inputString)
-    print(a)
-    indices = [i for i, x in enumerate(inputString) if x == "."]
-    print(indices)
+        for i in range(len(indices)):
+            b=[a[i:i+1]]
+            print(b)
+        return()
+
+    separateOctets(inputString)
 
     #print(verifyBlank(inputString))
     #print(verifyAlpha(inputString))
     #print(verifyLimit(inputString))
 
 
-#declarations
+#Declarations
 test="172.16.254.1"
 test2=""
 test3="172.a.13.34"
 test4="300"
 
 
-#function call
+#Function calls
 isIPv4Address(test)
 #isIPv4Address(tes2)
 #isIPv4Address(tes3)
