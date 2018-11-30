11677f605d0816fd9cb5e39b1623853540244112
Fabian <fabian.illanes@gmail.com>
2018-11-29 20:28:31 -0600

    new line

---
+++ minesweeper.py
@@ -0,0 +1,11 @@
+def minesweeper(matrix):
+    print(matrix)
+    print("len(matrix) =", len(matrix))
+    print("len(matrix[0]) = ", len(matrix[0]))
+
+
+#declarations
+m1=[[True,False,False],[False,True,False],[False,False,False]]
+
+#function calls
+minesweeper(m1)
