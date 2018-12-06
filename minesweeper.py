a6bb0d5febc36e8586df3693f1a8dea476f58dc0
Fabian <fabian.illanes@gmail.com>
2018-12-05 23:39:57 -0600

    new lines

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,19 @@
 def minesweeper(matrix):
+
+    r=[]
+
     print("matrix input =", matrix)
     print("len(matrix) =", len(matrix))
-    for i in range(0,len(matrix)):
+    for i in range(len(matrix)):
         print("len(matrix[", i, "]) = ", len(matrix[i]))
         print("matrix",i,matrix[i])
-        for j in range(0,len(matrix[i])):
+        for j in range(len(matrix[i])):
             print(matrix[i][j])
 
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            for x in range(-1,0,1):
+                for y in range(-1,0,1):
 
 #declarations
 m1=[[True,False,False],[False,True,False],[False,False,False]]
