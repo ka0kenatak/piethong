f7d9cd13e367765712538d4fcd3a8e0e537c4e9e
Fabian <fabian.illanes@gmail.com>
2018-12-14 17:55:06 -0600

    finished

--- evenDigitsOnly.py
+++ evenDigitsOnly.py
@@ -1,6 +1,20 @@
 def evenDigitsOnly(n):
 
+    s=str(n)
+    r=[]
+    for i in s:
+        r.append(int(i))
+
+    print("value of n is",n)
+    print("value of r is",r)
+
+    for i in r:
+        if i%2!=0:
+            return(False)
+    return(True)
 
 #Declaration
+num1=123456
 
 #Function calls
+evenDigitsOnly(num1)
