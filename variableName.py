934fc8ab242831101c39ec30df7e29df2b6374aa
Fabian <fabian.illanes@gmail.com>
2018-12-15 12:35:24 -0600

    finish

--- variableName.py
+++ variableName.py
@@ -4,16 +4,28 @@ def variableName(name):
     if name[0].isdigit()==True:
         print("False")
         return(False)
+
+    for val in name:
+        if (val=="-" or val==" "):
+            print("False")
+            return(False)
+        elif (val.isdigit()==False and val.isalpha()==False and val!="_"):
+            print("False")
+            return(False)
     print("True")
     return(True)
 
-    
+
 #Declarations
 var1="2asd3"
 var2="sadf-234"
 var3="asf_daf_13"
+var4="234 3"
+var5="234]"
 
 #Function calls
 variableName(var1)
 variableName(var2)
 variableName(var3)
+variableName(var4)
+variableName(var5)
