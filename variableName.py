b10b4b31db7ac57121013735e91dac8cb65941e2
Fabian <fabian.illanes@gmail.com>
2018-12-14 19:57:50 -0600

    new lines

---
+++ variableName.py
@@ -0,0 +1,19 @@
+def variableName(name):
+
+    print("input name is",name)
+    if name[0].isdigit()==True:
+        print("False")
+        return(False)
+    print("True")
+    return(True)
+
+    
+#Declarations
+var1="2asd3"
+var2="sadf-234"
+var3="asf_daf_13"
+
+#Function calls
+variableName(var1)
+variableName(var2)
+variableName(var3)
