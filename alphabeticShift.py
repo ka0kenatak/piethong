4bce47313713157a090278d45857e1be089e3c3a
Fabian <fabian.illanes@gmail.com>
2018-12-17 13:56:09 -0600

    trying dicts

---
+++ alphabeticShift.py
@@ -0,0 +1,17 @@
+def alphabeticShift(inputString):
+
+    alpha_dict={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7}
+
+    for val in inputString:
+        print(val)
+        print(alpha_dict[val]+1)
+
+
+
+
+#Declarations
+str1="abcdef"
+
+
+#Functional calls
+alphabeticShift(str1)
