39acd95316ca370d8dbc0e48ed922c8b92fa2ad0
Fabian Illanes <fillanes@rocketsoftware.com>
2018-12-20 16:25:57 -0600

    new lines

---
+++ depositProfit.py
@@ -0,0 +1,17 @@
+def depositProfit(deposit, rate, threshold):
+
+    balance=deposit
+    count=0
+    while (threshold/balance >= 1):
+        balance+=balance*(rate/100)
+        count+=1
+        print(balance)
+    return(count)
+
+#Declarations
+d1=100
+r1=20
+t1=170
+
+#Function Calls
+#depositProfit(d1,r1,t1)
