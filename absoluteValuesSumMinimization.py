c712bac78dbc35d1bc29c951b3e92bd0bb193a2b
Fabian <fabian.illanes@gmail.com>
2018-12-23 20:09:02 -0600

    initial commit

---
+++ absoluteValuesSumMinimization.py
@@ -0,0 +1,11 @@
+def absoluteValuesSumMinimization(a):
+
+    for i in range(len(a)):
+        abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+        #abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+
+
+#Declarations
+
+
+#Function Calls
