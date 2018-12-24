97a440e47f8af300eb3290f108bbca7404cecaca
Fabian <fabian.illanes@gmail.com>
2018-12-24 00:30:54 -0600

    finished

--- absoluteValuesSumMinimization.py
+++ absoluteValuesSumMinimization.py
@@ -1,11 +1,21 @@
 def absoluteValuesSumMinimization(a):
 
-    for i in range(len(a)):
-        abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
-        #abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
-
+    temp=1000000000
+    #print("a=",a)
+    for x in a:
+        result=0
+        for i in range(len(a)):
+            result+=abs(a[i]-x)
+            #print("abs(",i,"-",x,")=",result)
+        #print("total result with element",x,"is",result)
+        if (result < temp):
+            temp=result
+            r=x
+    #print("smallest result is with element",r)
+    return(r)
 
 #Declarations
-
+l1=[2,4,7]
 
 #Function Calls
+#absoluteValuesSumMinimization(l1)
