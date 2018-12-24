d52beaceb9615bc26cbbd895a805d2a84e6bf218
Fabian <fabian.illanes@gmail.com>
2018-12-24 12:08:17 -0600

    finished

---
+++ firstDigit.py
@@ -0,0 +1,11 @@
+def firstDigit(inputString):
+
+    for val in inputString:
+        if (val.isdigit()==True):
+            return(val)
+#Declarations
+s1="var_1__Int"
+s2="me_1_you_2_them_3"
+
+#Function Calls
+firstDigit(s2)
