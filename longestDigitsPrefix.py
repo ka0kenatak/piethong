bc4fe3fe012a385b932b9cc647e1b53da8ca96ab
Fabian <fabian.illanes@gmail.com>
2018-12-25 22:50:07 -0600

    more lines

--- longestDigitsPrefix.py
+++ longestDigitsPrefix.py
@@ -1,7 +1,19 @@
 def longestDigitsPrefix(inputString):
 
+    array=[]
+    if (inputString[0].isalpha==True or inputString[0]==" "):
+        newString=''.join(array)
+        print(newString)
+        return(newString)
+    for val in inputString:
+        print(type(val))
+        while(val.isdigit==True):
+            array.append(val)
+            print(array)
+    print(array)
 
 #Declarations
-
+s1="123aa1"
 
 #Function Calls
+longestDigitsPrefix(s1)
