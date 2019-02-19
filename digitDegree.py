596fbf7cf960d4324f22dfd651eba048d56421e8
Fabian <fabian.illanes@gmail.com>
2019-02-19 15:57:27 -0600

    new lines

--- digitDegree.py
+++ digitDegree.py
@@ -1,26 +1,32 @@
 def digitDegree(n):
 
-
     newString=str(n)
-    #print(newString)
 
     newArray=[]
     for i in newString:
         newArray.append(i)
 
-    #print(newArray)
     newNumber=0
     for val in newArray:
         newNumber+=int(val)
 
-    print(newNumber)
+    #Print Statements
+    print("n=",n)
+    print("newString=",newString)
+    print("newArray=",newArray)
+    print("newNumber = ",newNumber)
 
     if newNumber > 10:
         digitDegree(newNumber)
+
+
 #Declarations
 n1=100
 n2=91
+n3=10
 
-#Function Call
+
+#Function Calls
 #digitDegree(n1)
 digitDegree(n2)
+#digitDegree(n3)
