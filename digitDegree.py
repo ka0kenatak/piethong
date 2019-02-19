fd803485b2d3c95efd24379bb7c05f1be65a2bc9
Fabian <fabian.illanes@gmail.com>
2019-02-19 01:08:52 -0600

    try recursive

--- digitDegree.py
+++ digitDegree.py
@@ -1,10 +1,26 @@
 def digitDegree(n):
 
-    
 
+    newString=str(n)
+    #print(newString)
 
+    newArray=[]
+    for i in newString:
+        newArray.append(i)
+
+    #print(newArray)
+    newNumber=0
+    for val in newArray:
+        newNumber+=int(val)
+
+    print(newNumber)
+
+    if newNumber > 10:
+        digitDegree(newNumber)
 #Declarations
 n1=100
+n2=91
 
 #Function Call
-digitDegree(n1)
+#digitDegree(n1)
+digitDegree(n2)
