4b4e862f12098a47485f1fead5a760f151d1cc69
Fabian <fabian.illanes@gmail.com>
2019-08-07 00:05:51 -0500

    new lines

--- isBeautifulString.py
+++ isBeautifulString.py
@@ -1,1 +1,15 @@
 def isBeautifulString(inputString):
+
+    count = 0
+    for i in inputString:
+        print(i)
+
+
+
+#Declarations
+s1="abcedfg"
+s2="bbbaacdafe"
+s3=="aabbb"
+
+#Function calls
+isBeautifulString(s1)
