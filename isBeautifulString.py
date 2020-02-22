05ce612752881e8f483baea804c33a83d8a6ca43
Fabian <fabian.illanes@gmail.com>
2020-02-21 23:24:42 -0600

    done

--- isBeautifulString.py
+++ isBeautifulString.py
@@ -1,15 +1,5 @@
 def is_beautiful_string(inputString):
 
+    r = [inputString.count(i) for i in string.ascii_lowercase]
 
-    #newArray=[inputString.count(i) for i in string.ascii_lowercase]
-    #for i in inputString:
-    print(inputString.lower())
-
-
-#Declarations
-s1="abcedfg"
-s2="bbbaacdafe"
-s3="aabbb"
-
-#Function calls
-is_beautiful_string(s2)
+    return r[::-1] == sorted(r)
